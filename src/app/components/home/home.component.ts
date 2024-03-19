0ce6a6730610e9e3d8c531fd2193ec74694124bc
Mrvs Sai Kiran <83836566+mrvsaikiran@users.noreply.github.com>
2024-03-19 19:21:47 +0530

    updated home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,33 +1,40 @@
-import { Component } from '@angular/core';
-import { Router,RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router,RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { MovieService } from '../../Services/movie.service';
+import { Movie } from '../../Models/Movie';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,RouterModule,NavbarComponent],
+  imports: [CommonModule,RouterModule,NavbarComponent,MovieService, ActivatedRoute],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-  cities: {name:string }[] = [
-    { name: 'Mumbai' },
-    { name: 'Delhi' },
-    { name: 'Bengaluru' },
-    { name: 'Hyderabad' },
-    { name: 'Ahmedabad' },
-    { name: 'Chandigarh' },
-    { name: 'Chennai' },
-    { name: 'Pune' },
-    { name: 'Kolkata' },
-    { name: 'Kochi' }
-  ];
+export class HomeComponent implements OnInit{
 
-  constructor(private router: Router) {}
+  cities: string[] = [];
+  movies: Movie[] =[];
+  selectedCity: string = "";
+  constructor(private router: Router, private service: MovieService, private activatedRoute: ActivatedRoute) {
+    activatedRoute.paramMap.subscribe((params:any) => {
+        params.get("city")
+    })
+  }
 
   navigateToAllCities() {
     this.router.navigate(['/home']);
   }
 
+  ngOnInit() {
+    this.service.getAllCities().subscribe((cities: string[]) =>
+      {
+        this.cities = cities;
+      });
+   }
+   onCitySelection(city: string) {
+      this.selectedCity = city;
+      this.service.getMovieDetails(city);
+   }
 }
